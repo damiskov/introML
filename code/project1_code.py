2d8089becd3fd5340d5ddaec6096629009c689ea
DavidMiles-Skov <milesskovd@gmail.com>
2024-02-15 22:43:07 +0100

    more summary stats, PCA.

--- code/project1_code.py
+++ code/project1_code.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn as skl
 from scipy.linalg import svd
+import scipy.stats as stat
 import sys
 
 
@@ -83,8 +84,98 @@ def barplots(save_figs=False):
         plt.show()
 
 
+# General summary statistics
+        
+def continuous_summary_stats():
+
+    for col_name, col_data in zip(continuous_cols, data_drop_missing_obs[:, continuous_idx].T):
+
+        print(col_name)
+        print("Mean: " + str(round(np.mean(col_data), 2)))
+        print(f"Median: {round(np.median(col_data), 2)}")
+        print("Standard deviation:chi "+ str(round(np.std(col_data), 2)))
+        print(f"25% percentile: {str(round(np.percentile(col_data, 25), 2))}") 
+        print(f"75% percentile: {str(round(np.percentile(col_data, 75), 2))}")
+
+    return None
+
+
+def categorical_summary_stats():
+    for col_name, col_data in zip(cat_cols, data_drop_missing_obs[:, cat_idx].T):
+        categories = np.array(list(set(col_data)))
+        counts = np.array([np.count_nonzero(col_data==i) for i in categories])
+        print(col_name)
+        print(categories)
+        print(f"Percentages: {np.around(100*counts/sum(counts), 2)}")
+
+
+
 # histograms()
 # barplots()
+# continuous_summary_stats()
+# categorical_summary_stats()      
+
             
 # PCA
-            
+        
+def PCA(save_figs=False):
+    X = data_drop_missing_obs[:, continuous_idx]
+    y = data_drop_missing_obs[:, -1]
+    print(set(y))
+    print(X.shape, y.shape)
+    N, M = X.shape[0], X.shape[1]
+    C = 5
+
+    # Standardising data
+
+    X_s = (X - np.ones((N, 1))*X.mean(axis=0))/X.std(axis=0)
+
+    # Computing SVD
+    
+    U, S, V = svd(X_s, full_matrices=False)
+    rho = (S * S) / (S * S).sum()
+
+    threshold=0.9
+    # Plot variance explained
+    plt.figure()
+    plt.plot(range(1, len(rho) + 1), rho, "x-")
+    plt.plot(range(1, len(rho) + 1), np.cumsum(rho), "o-")
+    plt.plot([1, len(rho)], [threshold, threshold], "k--")
+    plt.title("Variance explained by principal components")
+    plt.xlabel("Principal component")
+    plt.ylabel("Variance explained")
+    plt.legend(["Individual", "Cumulative", "Threshold"])
+    plt.grid()
+
+    if save_figs:
+        path = "figures/PCA/cumvar.png"
+        plt.savefig(path, dpi=300)
+
+    plt.show()
+
+    # Project the centered data onto principal component space
+    Z = X_s @ V.T
+
+    # Indices of the principal components to be plotted
+    i = 0
+    j = 1
+
+    # Plot PCA of the data
+    f = plt.figure()
+    plt.title("Cleveland Heart Disease: PCA")
+    # Z = array(Z)
+    for c in range(C):
+        # select indices belonging to class c:
+        class_mask = y == c
+        plt.plot(Z[class_mask, i], Z[class_mask, j], "o", alpha=0.5)
+    plt.legend([str(i) for i in range(5)])
+    plt.xlabel("PC{0}".format(i + 1))
+    plt.ylabel("PC{0}".format(j + 1))
+    if save_figs:
+        path = "figures/PCA/projected.png"
+        plt.savefig(path, dpi=300)
+
+    plt.show()
+
+
+# PCA()
