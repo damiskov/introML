c9a350522da99bef112ead213191e2d416d34be0
DavidMiles-Skov <85405189+DavidMiles-Skov@users.noreply.github.com>
2024-05-11 19:06:57 +0200

    Update bayesian_classifiers.py
    NaiveBayesClassifier can now handle multi-class problems (not just binary). Added example usage

--- exam-scripts/bayesian_classifiers.py
+++ exam-scripts/bayesian_classifiers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from string_format import str_to_binary_nparray
 
 def bayesClassifier(table, labels, X, y):
 
@@ -10,29 +11,26 @@ def bayesClassifier(table, labels, X, y):
 
 def NaiveBayesClassifier(X, y, f_index, f_labels, y_test):
     """
-    Given a table of binary values and labels, and a new observation X, 
-    return the probability of an observation with X_test and y_test 
-    using Naive Bayes classifier.
 
-    p(y=y_test | f_1=f_labels[0], ..., f_n=f_labels[n-1])  etc.
+    Performs the Naive Bayes Classifier on a given table of binary values and labels.
     
-    Parameters
+    Parametersa
     ----------
     X: nparray
         The table of binary values. (transactions)
     y: nparray
-        The labels of the data.
-    f_index: nparray
-        The indices of the features to consider. (f_1, f_2, ..., 0-indexed)
-    f_labels: nparray
+        The associated classes of each observation.
+    feature_idx: nparray
+        The indices of the features to consider. (f_1, f_2, ..., 0-indexed!)
+    feature_vals: nparray
         The labels of the features to consider. (f_1=0, f_2=1, ... etc)
-    y_test: int, the label of the test observation.
+    class_prediction: int, the label of the test observation.
 
-    (Prints)
+    Returns:
     -------
     float
-        The probability of the test observation with the given attribute values.
-
+        The probability that an observation, given features (feature_idx) with 
+        values (feature_vals), belongs to a given class (class_prediction).
     """
     
 
@@ -41,19 +39,36 @@ def NaiveBayesClassifier(X, y, f_index, f_labels, y_test):
     p_y = np.sum(y==y_test)/len(y)
     p_not_y = np.sum(y!=y_test)/len(y)
 
-    # Calculate the likelihoods
 
-    p_x_given_y = []
-    p_x_given_not_y = []
+    # Numerator
 
-    for i in range(len(f_index)):
-        p_x_given_y.append(np.sum(X[y==y_test][:,f_index[i]] == f_labels[i])/np.sum(y==y_test))
-        p_x_given_not_y.append(np.sum(X[y!=y_test][:,f_index[i]] == f_labels[i])/np.sum(y!=y_test))
-    
+    num = np.zeros(len(f_index))
+    for j, fidx in enumerate(f_index):
+        for i in range(len(X)):
+            if X[i][fidx] == f_labels[j] and y[i] == y_test:
+                num[j] += 1
+        num[j] /= np.sum(y==y_test)
 
+    num = np.prod(num)*p_y
 
+    # Denominator
 
-    NB_prob = (p_y * np.prod(p_x_given_y) )/(np.prod(p_x_given_not_y)*p_not_y + p_y * np.prod(p_x_given_y))
+    den = np.zeros(len(np.unique(y)))
+    for i, label in enumerate(np.unique(y)):
+        temp = np.zeros(len(f_index))
+        for j, fidx in enumerate(f_index):
+            temp_f = 0
+            for k in range(len(X)):
+                if X[k][fidx] == f_labels[j] and y[k] == label:
+                    temp_f += 1
+            temp_f /= np.sum(y==label)
+            temp[j] = temp_f
+        den[i] = np.prod(temp)*np.sum(y==label)/len(y)
+
+    den = np.sum(den)
+
+    NB_prob = num/den
+
 
     print(f"Probability of y={y_test}")
     print(f"Given:")
@@ -61,3 +76,31 @@ def NaiveBayesClassifier(X, y, f_index, f_labels, y_test):
         print(f"\tf_{f_index[i]+1}={f_labels[i]}")
     
     print(f"p: {round(NB_prob, 3)}")
+    return NB_prob
+
+
+
+if __name__=="__main__":
+
+    print(" Example usage (Question 20, Fall 2017):\n")
+    transactions = """1 0 1 0 1 0 1 0
+    1 0 1 0 1 0 1 0
+    1 0 1 0 1 0 1 0
+    1 0 1 0 1 0 0 1
+    1 0 1 0 0 1 0 1
+    1 0 0 1 0 1 1 0
+    0 1 1 0 0 1 0 1
+    0 1 1 0 1 0 0 1
+    0 1 0 1 1 0 1 0
+    0 1 0 1 0 1 1 0
+    """
+    """
+    The above must be converted to a numpy array of integers.
+    """
+    transactions = str_to_binary_nparray(transactions)
+    print("\n-----Question 20------\n")
+    labels = np.array([2,1,1,2,3,1,3,3,1,3])
+    feature_idx = np.array([1, 2])
+    feature_vals = np.array([1, 1])
+    class_prediction = 3
+    NaiveBayesClassifier(transactions, labels, feature_idx, feature_vals, class_prediction)
